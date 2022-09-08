e667a87022154569677f0385ebbad81239e62c1e
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2022-09-08 18:53:26 +0300

    Update Lab2z3.cpp

--- Lab2z3.cpp
+++ Lab2z3.cpp
@@ -1,7 +1,8 @@
-﻿#include <vector>
+#include <vector>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
+#include <fstream>
 
 //Окей, мы просто играем в жизнь
 //Часто не понимая роль
@@ -11,16 +12,19 @@ class Shop
 {
 private:
     std::string title;//Название магазина
+    std::string address;//Адрес магазина
     int year;//Год основания
     int number;//Номер телефона
     int networth;//Суммарная прибыль
 public:
     //КОНСТРУКТОР
-    Shop(std::string _title = "Название не указано", 
+    Shop(std::string _title = "Название не указано",
+        std::string _address = "Адрес не указан",
         int _year = 000, 
         int _number = 000, 
         int _networth = 000) {
         title = _title;
+        address = _address;
         year = _year;
         number = _number;
         networth = _networth;
@@ -32,6 +36,11 @@ public:
             std::string tmpx;
             std::cin >> tmpx;
             title = tmpx;
+
+            std::cout << "Введите адрес: " << std::endl;
+            std::string tmpk;
+            std::cin >> tmpk;
+            title = tmpk; 
 
             std::cout << "Введите год: " << std::endl;
             int tmpy;
@@ -52,22 +61,44 @@ public:
     
     //МЕТОД ДЛЯ ОТОБРАЖЕНИЯ МАГАЗИНА
     void displayShop() {
-        std::cout << title << "\n" 
+        std::cout << title << "\n"
+            <<"Адрес: "<<address<<"\n"
             << year << " года основания\n" 
             << "Номер телефона: " << number << "\n"
             << "Суммарная прибыль: " << networth<<" руб."<<"\n"<<std::endl;
+    }
+    //ЗАПИСЬ ОБЪЕКТА В ФАЙЛ
+    void seriallize(const std::string filename) {
+        std::ofstream fout;
+        fout.open(filename);
+            fout <<title<<"\n"<< year << "\n"<<number <<"\n"<< networth;
+            fout.close();
+    }
+    //ЧТЕНИЕ ОБЪЕКТА ИЗ ФАЙЛА
+    void deseriallize(const std::string filename) {
+        std::ifstream fin;
+        fin.open(filename);
+        fin >> title >> year >> number >> networth;
+        fin.close();
     }
 };
 int main()
 {
     setlocale(LC_ALL, "Russian");
     std::vector<Shop> shop;
-    Shop first("Пятерочка", 2003, +720132103, 28000000);
-    first.displayShop();
-    Shop a;
-    a.setter();
-    a.displayShop();
+    std::cout << "Пример элемента класса Shop: ";
+    Shop example("Пятерочка","Арбат  24", 1998, 88005555505, 178000000000);
+    shop.push_back(example);
+    example.displayShop();
+    example.seriallize("file.txt");
+    Shop first;
+    first.setter();
     shop.push_back(first);
-    shop.push_back(a);
+    std::cout << "Вы ввели: "<<std::endl;
+    first.displayShop();
+    Shop second;
+    second.setter();
+    shop.push_back(second);
+    std::cout << "Вы ввели: " << std::endl;
+    second.displayShop();
 }
-
