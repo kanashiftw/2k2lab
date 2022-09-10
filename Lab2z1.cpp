eff8c1ad449a5de8dc41b63a56a285ffbf6b5f29
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2022-09-10 16:31:59 +0300

    Update Lab2z1.cpp

--- Lab2z1.cpp
+++ Lab2z1.cpp
@@ -2,33 +2,38 @@
 #include <iostream>
 #include <sstream>
 #include <vector> 
-const int arr_size_x = 5;
-const int arr_size_y = 5;
+//Метод вычисления факториала
 int  fact(int N)
 {
-    if (N < 0)
-        return 0;
-    if (N == 0)
-        return 1;
-    else
-        return N * fact(N - 1);
+    if (N < 0) return 0;   
+    if (N == 0) return 1;   
+    else return N * fact(N - 1);
 }
+//Класс матрикс
 class Matrix {
 private:
     int row = 0;
     int col = 0;
     int * mass;
 public:
+    //Конструктор без параметров
     Matrix() {
         row = 0;
         col = 0;
         mass = NULL;
     }
+    //Конструктор лего
+    Matrix(int _col) {
+        col = _col;
+        mass = new int[col];
+    }
+    //Передаю привет Михаилу Милюшникову
     Matrix(int _row, int _col) {
         row = _row;
         col = _col;
         mass = new int[row * col];
     }
+    //Хочу пельменей
     void generate() {   
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col;j++) {
@@ -36,57 +41,79 @@ public:
             }
         }
     }
+    //А они меня нет АхаХХАХаХАХАХ=)
     int at(int i, int j) {
         return mass[i * j];
     }
+    //Ладно, следующий комментарий будет нормальным
     void SetAt(int i, int j) {
         int tmp;
         std::cout << "Введите желаемое значение"<<std::endl;
         std::cin >> tmp;
         mass[i * j] = tmp;
     }
-    void generateFin(Matrix a) {
-        for (int i = 0; i < row; i+=2) {
-            for (int j = 0; j < col; j++) {
-                mass[i] = a.mass[i * j];
+    //Генерация матрицы
+    Matrix generateFin(Matrix a) {
+        Matrix y(15);
+        int k = 0;
+            for (int i = 0; i < a.row; i += 2) {
+                for (int j = 0; j < a.col; j++) {
+                    y.mass[k] = a.mass[i * j];     
+                    k++;
+                }
+            }
+        return y;
+    }
+    //Выводы напрашиваются сами собой
+    void printMatrix() {
+        if (row <= 1) {
+            std::cout << "Сгенерированная матрица: " << std::endl<<std::endl;
+            for (int i = 0; i < col; i++) {
+                    std::cout << std::setw(8) << mass[i];
+                    if (i == 4 || i == 9 || i == 14) {
+                        std::cout<<"\n";
+                }
             }
         }
-    }
-    void printMatrix() {
-        std::cout << "Сгенерированная матрица: " << std::endl;
-        for (int i = 0; i < row; i++) {
-            std::cout << "\n";
-            for (int j = 0; j < col; j++) {
-                if (i == 0 && j == 0) {
-                    std::cout << std::setw(8)<< mass[i * j];
-                }
-                else {
+        //Ельсе
+        else {
+            std::cout << "Сгенерированная матрица: " << std::endl;
+            for (int i = 0; i < row; i++) {
+                std::cout << "\n";
+                for (int j = 0; j < col; j++) {
                     std::cout << std::setw(8) << mass[i * j];
                 }
             }
+            std::cout << "\n";
         }
-        
     }
 };
+//класс Виктор, а почему не Сашок=)
 class Vector {
 private:
     std::vector<Matrix> listofmatrixes;
 public:
+    //Виктор на легке
     Vector() {
         this->listofmatrixes = listofmatrixes;
     }
+    //Виктор приблатненный сегодня
     Vector(Matrix a) {
         this->listofmatrixes = listofmatrixes;
         this->listofmatrixes.push_back(a);
     }
+    //инт оператор квадратный
+    int& operator [] (int a);
     ~Vector() {}
 };
-
+//главный(Я)
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    Matrix z(arr_size_x, arr_size_y);
+    Matrix z(5, 5);
     z.generate();
     z.printMatrix();
     Vector a(z);
+    Matrix o=z.generateFin(z);
+    o.printMatrix();
 }
