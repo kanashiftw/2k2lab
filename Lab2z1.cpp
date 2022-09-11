e349d668e5a22d997cbf25475d95a1b1b9b36382
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2022-09-11 16:28:45 +0300

    Update Lab2z1.cpp

--- Lab2z1.cpp
+++ Lab2z1.cpp
@@ -5,16 +5,36 @@
 //Метод вычисления факториала
 int  fact(int N)
 {
-    if (N < 0) return 0;   
+    if (N < 0) return -1;
     if (N == 0) return 1;   
     else return N * fact(N - 1);
 }
 //Класс матрикс
-class Matrix {
+class Matrix{
 private:
     int row = 0;
     int col = 0;
-    int * mass;
+    int** mass;
+    //Метод инициализации матрицы
+    void init()
+    {
+        mass = new int* [col];
+        for (int i = 0; i < col; i++)
+        {
+            mass[i] = new int[row];
+        }
+    }
+    //Метод заполнения матрицы
+    void fill()
+    {
+        for (int i = 0; i < col; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                mass[i][j] = fact(i+j);
+            }
+        }
+    }
 public:
     //Конструктор без параметров
     Matrix() {
@@ -22,35 +42,30 @@ public:
         col = 0;
         mass = NULL;
     }
-    //Конструктор лего
+    //Матрица
     Matrix(int _col) {
+        row = 1;
         col = _col;
-        mass = new int[col];
+        init();
+        fill();
     }
-    //Передаю привет Михаилу Милюшникову
+    //Двумерная матрица
     Matrix(int _row, int _col) {
         row = _row;
         col = _col;
-        mass = new int[row * col];
+        init();
+        fill();
     }
-    //Хочу пельменей
-    void generate() {   
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col;j++) {
-                mass[i * j] = fact(i + j);
-            }
-        }
-    }
-    //А они меня нет АхаХХАХаХАХАХ=)
+    //Метод поиска значения по индексу
     int at(int i, int j) {
-        return mass[i * j];
+        return mass[i][j];
     }
-    //Ладно, следующий комментарий будет нормальным
+    //Метод замены значения по индексу
     void SetAt(int i, int j) {
         int tmp;
         std::cout << "Введите желаемое значение"<<std::endl;
         std::cin >> tmp;
-        mass[i * j] = tmp;
+        mass[i][j] = tmp;
     }
     //Генерация матрицы
     Matrix generateFin(Matrix a) {
@@ -58,62 +73,72 @@ public:
         int k = 0;
             for (int i = 0; i < a.row; i += 2) {
                 for (int j = 0; j < a.col; j++) {
-                    y.mass[k] = a.mass[i * j];     
+                    y.mass[k][0] = a.mass[i][ j];
                     k++;
                 }
             }
         return y;
     }
-    //Выводы напрашиваются сами собой
+    //Метод вывода матрицы в консоль
     void printMatrix() {
-        if (row <= 1) {
+        if (row <= 1) { //Матрица с одной строкой
             std::cout << "Сгенерированная матрица: " << std::endl<<std::endl;
             for (int i = 0; i < col; i++) {
-                    std::cout << std::setw(8) << mass[i];
+                    std::cout << std::setw(8) << mass[i][0];
                     if (i == 4 || i == 9 || i == 14) {
                         std::cout<<"\n";
                 }
             }
         }
-        //Ельсе
-        else {
+        else { //Матрица многа строк
             std::cout << "Сгенерированная матрица: " << std::endl;
             for (int i = 0; i < row; i++) {
                 std::cout << "\n";
                 for (int j = 0; j < col; j++) {
-                    std::cout << std::setw(8) << mass[i * j];
+                    std::cout << std::setw(8) << mass[i][j];
                 }
             }
             std::cout << "\n";
         }
     }
+    //Деструктор класса
+    ~Matrix() {}
 };
-//класс Виктор, а почему не Сашок=)
+//класс Вектор
 class Vector {
 private:
     std::vector<Matrix> listofmatrixes;
 public:
-    //Виктор на легке
+    //Без параметров
     Vector() {
         this->listofmatrixes = listofmatrixes;
     }
-    //Виктор приблатненный сегодня
+    //Не без параметров
     Vector(Matrix a) {
         this->listofmatrixes = listofmatrixes;
         this->listofmatrixes.push_back(a);
     }
-    //инт оператор квадратный
+ 
+    //Последнее значение из вектора
+    void Printv(Vector z) {
+        for (auto i : listofmatrixes)
+            i.printMatrix();
+    }
+    //Оператор инициализации
     int& operator [] (int a);
-    ~Vector() {}
+    //Деструктор класса
+    ~Vector(){}
 };
-//главный(Я)
 int main()
 {
     setlocale(LC_ALL, "Russian");
     Matrix z(5, 5);
-    z.generate();
     z.printMatrix();
     Vector a(z);
     Matrix o=z.generateFin(z);
     o.printMatrix();
+    std::cout << "Содержимое вектора: "<<std::endl;
+    a.Printv(a);
+    a.~Vector();
+    z.~Matrix();
 }
